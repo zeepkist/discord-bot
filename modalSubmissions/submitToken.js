94749fd333ec097851a79f8cb551f55e922d6a62
wopian <wopian@wopian.me>
2023-01-19 01:41:52 +0000

    Update production to output generated at 87de6d2

--- modalSubmissions/submitToken.js
+++ modalSubmissions/submitToken.js
@@ -9,7 +9,7 @@ import { alreadyLinkedReply } from '../components/accountAlreadyLinked.js'
 import { database } from '../services/database.js'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
-const privateKey = readFileSync(join(__dirname, '../../private.pem'), 'utf8')
+const privateKey = readFileSync(join(__dirname, '../private.pem'), 'utf8')
 const decryptSteamId = token =>
   privateDecrypt(
     { key: privateKey, padding: constants.RSA_PKCS1_OAEP_PADDING },
