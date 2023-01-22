dfc48719c803f7c8bff025f6b8d082a52507366e
wopian <wopian@wopian.me>
2023-01-22 02:12:31 +0000

    ci: move everything back into one job

--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,3 @@ export const STEAM_API_URL = 'https://api.steampowered.com/'
 export const GITHUB_API_URL = 'https://api.github.com/'
 
 export const IS_PRODUCTION = Boolean(process.env.ZEEPKIST_BOT_PRODUCTION)
-
-console.log(IS_PRODUCTION)
