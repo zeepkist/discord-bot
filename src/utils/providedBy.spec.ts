d3a436cb17a40766bc3ba005f035202efb9f5e51
wopian <wopian@wopian.me>
2023-01-22 02:13:37 +0000

    test: remove unused import

--- src/utils/providedBy.spec.ts
+++ src/utils/providedBy.spec.ts
@@ -1,6 +1,5 @@
 import test from 'ava'
 
-import { LevelRecord } from '../models/record.js'
 import { providedBy } from './index.js'
 
 const macro = test.macro((t, expected: string) => t.is(providedBy, expected))
