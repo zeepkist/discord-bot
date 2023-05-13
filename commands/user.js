f6a0022232c9e7ee19c1300d3c2feac9ba13548e
wopian <wopian@users.noreply.github.com>
2023-05-13 01:07:15 +0000

    v1.23.0 199682f

--- commands/user.js
+++ commands/user.js
@@ -1,14 +1,19 @@
-import { getUser, getUserBySteamId } from '@zeepkist/gtr-api';
-import { ApplicationCommandOptionType, ApplicationCommandType, EmbedBuilder, inlineCode } from 'discord.js';
+import { getUser, getUserByDiscordId, getUserBySteamId } from '@zeepkist/gtr-api';
+import { ApplicationCommandOptionType, ApplicationCommandType } from 'discord.js';
 import { userEmbed } from '../components/embeds/userEmbed.js';
 import { userNotFoundEmbed } from '../components/embeds/userNotFoundEmbed.js';
-import { database } from '../services/database.js';
 import { log } from '../utils/index.js';
 export const user = {
     name: 'user',
     description: 'Get information about a user.',
     type: ApplicationCommandType.ChatInput,
     options: [
+        {
+            name: 'user',
+            description: 'Discord User',
+            type: ApplicationCommandOptionType.User,
+            required: false
+        },
         {
             name: 'steamid',
             description: "User's Steam ID.",
@@ -27,36 +32,22 @@ export const user = {
     ],
     ephemeral: false,
     run: async (interaction) => {
-        const linkedAccount = await database('linked_accounts')
-            .select('steamId')
-            .where({
-            discordId: interaction.user.id
-        });
-        log.info(`Found ${linkedAccount.length} linked accounts.`, interaction);
-        let steamId = interaction.options.data.find(option => option.name === 'steamid')?.value;
+        let discordUser = interaction.options.data.find(option => option.name === 'user')?.user;
+        const steamId = interaction.options.data.find(option => option.name === 'steamid')?.value;
         const id = interaction.options.data.find(option => option.name === 'id')
             ?.value;
-        log.info(`Steam ID: ${steamId}, ID: ${id}`, interaction);
-        if ((!linkedAccount || linkedAccount.length === 0) && !steamId && !id) {
-            log.info('No linked account or option arguments provided. Ending interaction.', interaction);
-            const embed = new EmbedBuilder()
-                .setColor(0xff_92_00)
-                .setTitle('User not linked')
-                .setDescription(`You must provide either a Steam ID or a user ID.\n\nIf you link your Steam account with ${inlineCode('/verify')}, you can use this command without providing a Steam ID or user ID.`)
-                .setTimestamp();
-            await interaction.editReply({
-                embeds: [embed]
-            });
-            return;
-        }
-        if (!steamId && !id) {
-            steamId = linkedAccount[0].steamId;
-            log.info(`Using linked account Steam ID: ${steamId}`, interaction);
+        log.info(`Discord ID: ${discordUser?.id}, Steam ID: ${steamId}, ID: ${id}`, interaction);
+        if (!discordUser?.id && !steamId && !id) {
+            discordUser = interaction.user;
         }
         try {
-            const user = steamId ? await getUserBySteamId(steamId) : await getUser(id);
+            const user = discordUser?.id
+                ? await getUserByDiscordId(discordUser.id)
+                : steamId
+                    ? await getUserBySteamId(steamId)
+                    : await getUser(id);
             log.info(`Found user: ${user.steamName}`, interaction);
-            await userEmbed(interaction, user);
+            await userEmbed(interaction, user, discordUser);
         }
         catch (error) {
             log.error(String(error), interaction);
